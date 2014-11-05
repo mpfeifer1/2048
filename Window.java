8f488f9971891b52a9891a70752ae72b05b0b773
mpfeifer <mikepf97@gmail.com>
2014-11-04 23:47:17 -0600

    Imported swing and started window definitions

--- Window.java
+++ Window.java
@@ -1,8 +1,27 @@
-public class Window {
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.FontMetrics;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+import javax.swing.ImageIcon;
+import javax.swing.JPanel;
+import javax.swing.Timer;
+
+public class Window extends JPanel{
 
 	/*This Class should Contain:
 	 *  An output system for the entire board
 	 */
+
+	private final int WIDTH = 440;
+	private final int HEIGHT = 440;
+	private final int TILE_SIZE =100
 
 	public Window {
 
